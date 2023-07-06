9637d1163d4edbb46c90ff90a66efb1a26108e3b
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2023-07-06 18:40:58 +0900

    feat: InputManager 대리자를 이용한 입력부 구현

--- WhoAreYou/Assets/Scripts/Manager/InputManager.cs
+++ WhoAreYou/Assets/Scripts/Manager/InputManager.cs
@@ -1,8 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager
 {
+    public Action<KeyCode> KeyAction = null;
 
+    public void OnUpdate()
+    {
+        if (KeyAction != null)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                KeyAction.Invoke(KeyCode.W);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                KeyAction.Invoke(KeyCode.A);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                KeyAction.Invoke(KeyCode.S);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                KeyAction.Invoke(KeyCode.D);
+            }
+        }
+    }
 }
